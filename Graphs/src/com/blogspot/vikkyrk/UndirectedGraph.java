a218a2811fffbdfcb33b9ee5f3d1dcf0f77c8ad0
vikasrk <vikky.rk@gmail.com>
2012-09-25 10:51:10 -0700

    Graph DS
    Undirected Graph
    DFS
    BFS
    Connected Components

---
+++ Graphs/src/com/blogspot/vikkyrk/UndirectedGraph.java
@@ -0,0 +1,57 @@
+package com.blogspot.vikkyrk;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class UndirectedGraph {
+
+	private ArrayList<LinkedList<Integer>> vertices = null;
+	private int numEdges = 0;
+	
+	public UndirectedGraph (int V) {
+		vertices = new ArrayList<LinkedList<Integer>>(V);
+		for(int i=0;i<V;i++) {
+			vertices.add(new LinkedList<Integer>());
+		}
+	}
+	
+	public UndirectedGraph(BufferedReader in) throws IOException {
+		this(Integer.parseInt(in.readLine()));
+		String s;
+		while( (s = in.readLine()) != null) {
+			String[] str = s.split(" ");
+			addEdge(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
+		}
+	}
+	
+	public int V() {
+		return vertices.size();
+	}
+	
+	public int E() {
+		return numEdges;
+	}
+	
+	public void addEdge(int v, int w) {
+		vertices.get(v).add(w);
+		vertices.get(w).add(v);
+		numEdges++;
+	}
+	
+	public Iterable<Integer> adj(int v) {
+		return vertices.get(v);
+	}
+
+	@Override
+	public String toString() {
+		String s = "";
+		s = s + "\n Graph \n";
+		for(int i=0; i<vertices.size(); i++) {
+			s = s + i + ":";
+			s = s + vertices.get(i) + "\n";
+		}
+		return s;
+	}
+}
---
+++ Graphs/src/com/blogspot/vikkyrk/mainTest.java
@@ -0,0 +1,50 @@
+package com.blogspot.vikkyrk;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class mainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File f = new File("/media/STUDY/Tech/AlgoWrkSpc/Graphs/src/undirectedGraphInput.txt");
+		FileReader fR = null;
+
+		try {
+			fR = new FileReader(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		UndirectedGraph myGraph = null;
+		try {
+			myGraph = new UndirectedGraph(new BufferedReader(fR));
+		} catch (IOException e) {
+			throw new RuntimeException("Error Creating Graph");
+		}
+		
+		System.out.println(myGraph);
+		
+		/*DepthFirstSearch dfs = new DepthFirstSearch(myGraph,1);
+		System.out.println("Dfs count for vertex 1 = " + dfs.count());
+		
+		System.out.println("Path:" + dfs.path(3));
+		*/
+		
+		BreadthFirstSearch bfs = new BreadthFirstSearch(myGraph,1);
+		System.out.println("Dfs count for verted 1 = " + bfs.count());
+		
+		System.out.println("Path:" + bfs.path(3));
+		
+		ConnectedComponents cc = new ConnectedComponents(myGraph);
+		
+		System.out.println("Connected " + cc);
+		System.out.println("Connected " + cc.isConnected(0, 3) + " Num = " + cc.count());
+	}
+
+}
