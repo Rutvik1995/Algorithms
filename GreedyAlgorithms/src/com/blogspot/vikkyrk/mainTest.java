933188f7a7804e2998b1d7eddedd834c25581ade
vikasrk <vikky.rk@gmail.com>
2012-09-25 10:49:48 -0700

    Greedy Algorithms
    Still incomplete

---
+++ GreedyAlgorithms/src/com/blogspot/vikkyrk/mainTest.java
@@ -0,0 +1,13 @@
+package com.blogspot.vikkyrk;
+
+public class mainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ActivitySelection.ActivitySelectionSolve(10);
+	}
+
+}
